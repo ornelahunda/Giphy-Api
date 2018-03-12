896fe3b4a927cc2532f3db5184d6d21e0851027c
ornelahunda <ornelahunda@gmail.com>
2018-03-12 11:07:08 -0400

    adding links to the HTML file

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Giphy-Api
+
+The theme of this project are going to be giphy emotions.
---
+++ assets/javascript/script.js
@@ -0,0 +1,20 @@
+$(document).ready(function () {
+
+// Topics array is going to hold emotions Giphy
+    var topics = ["excited", "bored", "confused"];
+
+    // Click event listen listener
+    $("button").on("click", function () {
+
+
+
+    });
+
+
+
+
+
+
+
+}
+);
